c889a45cf44b33eb3eed038ed12ca5d5ceae8544
gmachin11 <machingonzalo11@gmail.com>
2022-07-07 21:24:09 -0300

    cambio de carpetas

--- TP 1 Gonzalo Machin.R
+++ TP 1 Gonzalo Machin.R
@@ -1,2 +1,14 @@
+rm(list = ls())
+
+gc()
+
 library(tidyverse)
 library(lubridate)
+library(sf)
+
+#Informacion sobre las paradas del transporte colectivo en Montevideo, Uruguay
+
+paradas_stm <- read_sf("data/v_uptu_paradas.shp",crs = 32721) #disponible en: https://catalogodatos.gub.uy/dataset/intendencia-montevideo-transporte-colectivo-paradas-y-puntos-de-control
+recorridos_stm <- read_sf("data/v_uptu_lsv.shp",crs = 32721) #disponible en: https://catalogodatos.gub.uy/dataset/intendencia-montevideo-transporte-colectivo-paradas-y-puntos-de-control
+
+head(paradas)
